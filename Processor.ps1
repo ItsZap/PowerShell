526ef0dcf89590c7a0e35618536fb3ddba8240e2
Riwut Libinuko <cakriwut@hotmail.com>
2016-09-11 15:44:19 +0800

    Remove \n from BodyText

---
+++ Processor.ps1
@@ -0,0 +1,8 @@
+﻿$sourceCSV = "d:\Temp\src.csv"
+$targetCSV = "d:\Temp\cleanSrc.csv"
+
+(Import-Csv $sourceCSV -Header "url","Desc description","BodyText") |% {
+    $_.BodyText = $_.BodyText -replace "\\n" , " "
+    $_
+} | Export-Csv $targetCSV -NoTypeInformation 
+
