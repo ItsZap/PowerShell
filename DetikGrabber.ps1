2042681a9e19a21b64a96cd48b7b2684754b17af
Riwut Libinuko <cakriwut@hotmail.com>
2016-09-11 10:48:43 +0800

    add ps
    Add ps

---
+++ DetikGrabber.ps1
@@ -0,0 +1,34 @@
+﻿#$targetXml = "C:\temp\detikcom_nasional.xml"
+$detikLink = "C:\temp\detiklink.txt"
+
+$targetRSS = "detikcom_nasional", "detikcom_internasional", "detikcom_bbc","detikcom_lapsus","detikcom_wawancara"," detikcom_prokontra"
+
+Remove-Item $detikLink -Force -ErrorAction Ignore
+
+$targetRSS |% {
+
+   $url = "http://rss.detik.com/index.php/" + $_
+   $targetXml = "C:\temp\" + $_ + ".xml"
+
+   Invoke-WebRequest -Uri $url -OutFile $targetXml -ErrorAction Stop
+
+   [xml]$Content = gc $targetXml
+   $Feed = $Content.rss.channel
+
+   $Feed.Item |% {
+   $msg = $_
+
+  # $Description = ($msg.description).Substring(0,($msg.description).IndexOf('<BR>'))
+
+    Write-host $msg.link
+    Add-Content $detikLink $msg.link
+
+
+   }
+}
+
+
+
+
+
+
